72d096dce6d3a2a6cb266e418bd5e4481b5e3b63
synergiance <lordmicro@gmail.com>
2014-04-17 16:56:36 -0400

    Make sure to compile in c++11 for what we will need to do, also started a mutex class

---
+++ cmutex.cpp
@@ -0,0 +1,19 @@
+/*
+ * cmutex.cpp
+ *
+ *  Created on 17 April 2014
+ *      By: Tim Gerety
+ */
+
+// Global includes
+#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <unistd.h>
+
+// Local includes
+#include "cmutex.h"
+
+using namespace std;
---
+++ cmutex.h
@@ -0,0 +1,25 @@
+/*
+ * cmutex.h
+ *
+ *  Created on 17 April 2014
+ *      By: Tim Gerety
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <unistd.h>
+
+using namespace std;
+
+#ifndef CMUTEX_H_
+#define CMUTEX_H_
+
+class CMutex
+{
+	//stuff
+};
+
+#endif /* CMUTEX_H_ */
