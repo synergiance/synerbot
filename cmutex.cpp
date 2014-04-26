10216e4010bde079e47d6fb0dcd7d214e4fabc83
synergiance <lordmicro@gmail.com>
2014-04-26 00:11:52 -0400

    Removed some old ugly code

--- cmutex.cpp
+++ cmutex.cpp
@@ -35,7 +35,6 @@ CMutex::CMutex(bool debug)
 void CMutex::push(string str)
 {// Pushes some data to the mutex buffer
     unique_lock<mutex> lck(mtx2);
-    //cout<<"Before Add Mutex\n";
     access(true, str);
     cv.notify_all();
 }
@@ -43,30 +42,23 @@ void CMutex::push(string str)
 bool CMutex::pull(string& str, int timeout) // Milliseconds
 {// Blocking function that conditionally blocks
 // Returns true if buffer is not empty
-    //cout<<"Before before remove\n";
     if (timeout >= 0)
     {
-        //cout<<"Before Unique lock\n";
         unique_lock<mutex> lck(mtx2);
-        //cout<<"After Unique lock\n";
         if (timeout > 0)
             cv.wait_for(lck,chrono::milliseconds(timeout));
         else
             cv.wait(lck);
     }
-    //cout<<"Before Remove Mutex\n";
     return access(false, str);
 }
 
 bool CMutex::access(bool adding, string& str)
 {// Our mutex locked function, returns true if the buffer is not empty
     bool bReturn = false;
-    //cout<<"MUTEX: Locking\n";
     mtx1.lock();
-    //cout<<"MUTEX: Locked\n";
     if (adding)
     {
-        //cout<<"Adding\n";
         strBuffer.push_back(str);
         bReturn = true;
         if (debugMode)
@@ -75,10 +67,8 @@ bool CMutex::access(bool adding, string& str)
     }
     else
     {
-        //cout<<"Checking\n";
         if (strBuffer.size() > 0)
         {
-            //cout<<"Removing\n";
             str = strBuffer[0];
             strBuffer.erase(strBuffer.begin());
             if (debugMode)
@@ -90,8 +80,6 @@ bool CMutex::access(bool adding, string& str)
         if (strBuffer.size() > 0)
             bReturn = true;
     }
-    //cout<<"MUTEX: Unlocking\n";
     mtx1.unlock();
-    //cout<<"MUTEX: Unlocked\n";
     return bReturn;
 }
