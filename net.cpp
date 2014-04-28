0074435d0421a44e4b77b714dd6af752c58f2755
synergiance <lordmicro@gmail.com>
2014-04-27 23:24:25 -0400

    Divide delay by 10

--- net.cpp
+++ net.cpp
@@ -108,7 +108,7 @@ void CNetSocket::toThread(string data)
 {// Send some data to the thread
     string str = data + "\r\n";
     write(pNet[1], str.c_str(), str.size() + 1);
-    usleep(100);
+    usleep(10);
 }
 
 void CNetSocket::main()
