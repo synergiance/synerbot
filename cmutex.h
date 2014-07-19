be423d84189e904366cfe3186a21042aab7098a1
synergiance <lordmicro@gmail.com>
2014-07-19 00:19:39 -0400

    Groundwork for message queue upgrade

--- cmutex.h
+++ cmutex.h
@@ -19,17 +19,29 @@ using namespace std;
 #ifndef CMUTEX_H_
 #define CMUTEX_H_
 
+struct CMutexMessage
+{
+    string command;
+    vector<string> command_arguments;
+};
+
 class CMutex
 {
 public:
     CMutex();
     CMutex(bool debug);
 
-    // Access array
+    // Access array -- Deprecating
     void push(string str);
     bool pull(string& str);
     bool pull(string& str, int timeout);
     bool pull(string& str, int timeout, int delay);
+
+    /* // Access array -- New
+    void push(CMutexMessage str);
+    bool pull(CMutexMessage& str);
+    bool pull(CMutexMessage& str, int timeout);
+    bool pull(CMutexMessage& str, int timeout, int delay); */
 private:
     // Array
     vector<string> strBuffer;
